ab2b7a1dd1bb37fd65fe153870cfdd706ee06d30
Norton <nortonsantos79@gmail.com>
2021-10-09 15:12:34 -0300

    included searchTerm in the state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: "",
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  render() {
     return (
       <div>
-        
+        <h1>WikiViewer</h1>
+
+        <a
+          className="random"
+          href="https://en.wikipedia.org/wiki/Special:Random"
+        >
+          Go to a random article
+        </a>
+
+        <input
+          type="text"
+          placeholder="Search with the Wikipedia API"
+          onChange={this.onSearchChange}
+        />
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     );
   }
-  
 }
 
 export default App;
