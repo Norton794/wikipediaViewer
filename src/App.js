171282223e2bc0302ed083a39ec177892146a23b
Norton <nortonsantos79@gmail.com>
2021-10-10 10:34:05 -0300

    getting the results from the api and stylized the page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const BASE = "https://en.wikipedia.org/w/api.php";
+const ACTION = "action=query";
+const LIST = "list=search";
+const SEARCH = "srsearch=";
+const DEFAULT_SEARCH = "React";
+const FORMAT = "format=json";
+const ORIGIN = "origin=*";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      list: [],
-      searchTerm: "React",
+      result: null,
+      searchTerm: DEFAULT_SEARCH,
     };
+    this.setSearchWiki = this.setSearchWiki.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchWiki(result) {
+    this.setState({ result });
   }
 
   onSearchChange(e) {
     this.setState({ searchTerm: e.target.value });
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(
+      `${BASE}?${ACTION}&${LIST}&${SEARCH}${searchTerm}&${FORMAT}&${ORIGIN}`
+    )
+      .then((response) => response.json())
+      .then((result) => this.setSearchWiki(result))
+      .catch((error) => error);
+  }
+
   render() {
-    const { list, searchTerm } = this.state;
+    const { result, searchTerm } = this.state;
+    if (!result) {
+      return null;
+    }
     return (
       <div className="page">
         <h1>WikiViewer</h1>
@@ -28,29 +55,32 @@ class App extends Component {
           Go to a random article
         </a>
 
-        <Search onChange={this.onSearchChange} value={searchTerm}> Search with the Wikipedia API</Search>
-        <List list={list}/>
+        <Search onChange={this.onSearchChange} value={searchTerm}>
+          Search with the Wikipedia API
+        </Search>
+        <List list={result.query.search} />
       </div>
     );
   }
 }
 
 const Search = ({ value, onChange, children }) => (
-  <input
-    type="text"
-    value={value}
-    onChange={onChange}
-    placeholder={children}
-  />
+  <input type="text" value={value} onChange={onChange} placeholder={children} />
 );
 
 const List = ({ list }) => {
   return (
-    <ul>
+    <div className="container">
       {list.map((item) => (
-        <li>{item.name}</li>
+        <div className="cards" key={item.pageid}>
+          <h3 className="pgTitle">{item.title}</h3>
+          <div
+            className="snippet"
+            dangerouslySetInnerHTML={{ __html: `<p>${item.snippet}</p>` }}
+          />
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
